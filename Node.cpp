9ad60966886536811af043af6425a9f24dbdbb58
moontails <dragontransformer@gmail.com>
2015-05-02 09:17:27 -0500

    Updating final versions of code and readme

--- Node.cpp
+++ Node.cpp
@@ -9,88 +9,94 @@
 #include "headers/MessageHandler.h"
 #include <algorithm>
 #include <sstream>
-#include <thread>
-#include <ctime>
-#include <chrono>
-#include <mutex>
-#include <map>
 #include <cmath>
+#include <typeinfo>
 
 std::vector<std::thread> thread_pool(9);
-std::vector<std::mutex> mtx_locks(9);
+std::vector<std::mutex> Node::mtx_locks(10);
 std::map<int,std::vector<int>> votingSet;
+std::chrono::system_clock::time_point beginning_of_time;
+
 //defining the static variables
 int Node::cs_int = 0;
 int Node::next_req = 0;
 int Node::total_exec_time = 0;
-std::vector<int> Node::member_list;
-std::vector<std::deque<std::string>> Node::messageQ(10);
+int Node::option = 0;
+std::vector<std::deque<MessageTimePair>> Node::messageQ(10);
 std::vector<std::vector<std::string>> Node::grantQ(10);
 
 void node_runner(const int id);
 
-// Function to pop safely from a node's message queue
-void pop_messageQ(const int node_id)
+//custom comparator to sort a vector of pairs based on the second element of the pair
+// and break ties based on thread ids.
+struct sort_pred
 {
-  mtx_locks[node_id].lock();
-  Node::messageQ[node_id].pop_front();
-  mtx_locks[node_id].unlock();
-}
-
-// Function to push safely to a node's message queue
-void push_messageQ(const int node_id, const std::string inputMessage, const State mode)
-{
-  mtx_locks[node_id].lock();
-
-  switch(mode)
+  bool operator()(const MessageTimePair &left, const MessageTimePair &right)
   {
-    case REQUEST:
-      Node::messageQ[node_id].push_back(inputMessage);
-      break;
-
-    case RELEASE:
-      Node::messageQ[node_id].push_front(inputMessage);
-      break;
+    if(left.second == right.second)
+    {
+      std::vector<std::string> inputMessageVector1,inputMessageVector2;
+      inputMessageVector1 = MessageHandler::deserialize(left.first);
+      inputMessageVector2 = MessageHandler::deserialize(right.first);
+      return std::stoi(inputMessageVector1[1]) < std::stoi(inputMessageVector2[1]);
+    }
+    else
+      return left.second < right.second;
   }
-
-  mtx_locks[node_id].unlock();
-}
+};
 
 // Function to pop safely from a node's message queue
-void clear_grantQ(const int node_id)
+void Node::pop_messageQ()
 {
-  mtx_locks[node_id].lock();
-  Node::grantQ[node_id].clear();
-  mtx_locks[node_id].unlock();
+  Node::mtx_locks[this->threadId].lock();
+  Node::messageQ[this->threadId].pop_front();
+  Node::mtx_locks[this->threadId].unlock();
 }
 
 // Function to push safely to a node's message queue
-void push_grantQ(const int node_id, const std::string inputMessage)
+void Node::push_messageQ(const int node_id, const MessageTimePair inputMessage, const State mode)
 {
-  mtx_locks[node_id].lock();
+  Node::mtx_locks[node_id].lock();
+  Node::messageQ[node_id].push_back(inputMessage);
+  std::sort(Node::messageQ[node_id].begin(), Node::messageQ[node_id].end(), sort_pred());
+  Node::mtx_locks[node_id].unlock();
+}
+
+// Function to pop safely from a node's message queue
+void Node::clear_grantQ()
+{
+  Node::mtx_locks[this->threadId].lock();
+  Node::grantQ[this->threadId].clear();
+  Node::mtx_locks[this->threadId].unlock();
+}
+
+// Function to push safely to a node's message queue
+void Node::push_grantQ(const int node_id, const std::string inputMessage)
+{
+  Node::mtx_locks[node_id].lock();
   Node::grantQ[node_id].push_back(inputMessage);
-  mtx_locks[node_id].unlock();
+  Node::mtx_locks[node_id].unlock();
 }
 
-// Function to display a vector
-void display(const std::vector<int>& v)
-{
-  std::cout << "Membership List: ";
-  for (int i=0; i<v.size();i++)
-  {
-    cout << v[i] << " ";
-  }
-  std::cout << std::endl;
-}
-
+// Node constructor
 Node::Node(const int id)
 {
+  ostringstream ss;
+
   this->threadId = id;
   this->myState = INIT;
-  this->requestSet = votingSet[id+1];
+  this->requestSet = votingSet[id];
   this->voted = false;
+  this->expired = false;
+  this->logged = false;
+  for (int i=0; i<this->requestSet.size();i++)
+  {
+    ss << this->requestSet[i] << " ";
+  }
+  this->requestSetString = ss.str();
 }
 
+//Function to display the voting set of each node
 void displaySet(const std::vector<int>& v)
 {
   std::cout << "Membership List: ";
@@ -101,84 +107,136 @@ void displaySet(const std::vector<int>& v)
   std::cout << std::endl;
 }
 
+// Function to display a message when threads are initialized
 void Node::display()
 {
-  std::cout << "Hi I am node: " << this->threadId << " with quorum as " << std::endl;
-  displaySet(this->requestSet);
+  std::cout << "Hi I am node: " << this->threadId << " with quorum: " << this->requestSetString << std::endl;
 }
 
+//Function to send a mutlicast message to all nodes in the voting set
 void Node::multicast(const State msgType)
 {
   std::string message;
   ostringstream ss;
-
+  MessageTimePair temp;
+  std::time_t logTime;
   switch(msgType)
   {
     case REQUEST:
+      logTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+      //std::cout << "Sending Request from " << this->threadId << " at " << logTime << std::endl;
       ss << "request " << this->threadId;
+
+      temp.first = MessageHandler::serialize(ss.str());
+      temp.second = std::chrono::system_clock::now();
 
       for(int i = 0; i < this->requestSet.size(); i++)
       {
-        push_messageQ(this->requestSet[i], MessageHandler::serialize(ss.str()), msgType);
+        if(Node::option == 1)
+        {
+          logTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+          std::cout << "Time: " << ctime(&logTime) << ", ThreadId: " << this->requestSet[i] << ", From: " << this->threadId << ", MessageType: " << temp.first << std::endl;
+        }
+        this->push_messageQ(this->requestSet[i], temp, msgType);
       }
       break;
 
     case RELEASE:
       ss << "release " << this->threadId;
 
+      temp.first = MessageHandler::serialize(ss.str());
+      temp.second = beginning_of_time;
+
       for(int i = 0; i < this->requestSet.size(); i++)
       {
-        push_messageQ(this->requestSet[i], MessageHandler::serialize(ss.str()), msgType);
+        if(Node::option == 1)
+        {
+          logTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+          std::cout << "Time: " << ctime(&logTime) << ", ThreadId: " << this->requestSet[i] << ", From: " << this->threadId << ", MessageType: " << temp.first << std::endl;
+        }
+        this->push_messageQ(this->requestSet[i], temp, msgType);
       }
       break;
   }
 }
 
+//Function to check last voted
+bool Node::check_last_voted(const MessageTimePair left, const MessageTimePair right)
+{
+  if(left.second == right.second)
+  {
+    std::vector<std::string> inputMessageVector1,inputMessageVector2;
+    inputMessageVector1 = MessageHandler::deserialize(left.first);
+    inputMessageVector2 = MessageHandler::deserialize(right.first);
+    return std::stoi(inputMessageVector1[1]) < std::stoi(inputMessageVector2[1]);
+  }
+  else
+    return left.second < right.second;
+}
+
+//Function to check a nodes message queue and take the necessary actions
 void Node::check_messages()
 {
   std::string inputMessage, outputMessage;
   std::vector<std::string> inputMessageVector;
+  MessageTimePair temp;
   //ostringstream ss;
+  std::time_t logTime;
 
   if(!Node::messageQ[this->threadId].empty())
   {
-    inputMessageVector = MessageHandler::deserialize(Node::messageQ[this->threadId].front());
+    temp = Node::messageQ[this->threadId].front();
+    inputMessageVector = MessageHandler::deserialize(temp.first);
     //ss << "Granted";
     if(inputMessageVector[0] == "request" && this->voted == false)
     {
-      this->voted = true;
-      push_grantQ(std::stoi(inputMessageVector[1]), "grant");
-      pop_messageQ(this->threadId);
+      if(this->voted == false)
+      {
+        //std::cout << "Sending Grant to " << inputMessageVector[1] << std::endl;
+        this->voted = true;
+        if(Node::option == 1)
+        {
+          logTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+          std::cout << "Time: " << ctime(&logTime) << ", ThreadId: " << inputMessageVector[1] << ", From: " << this->threadId << ", MessageType: grant" <<  std::endl;
+        }
+        this->push_grantQ(std::stoi(inputMessageVector[1]), "grant");
+        this->pop_messageQ();
+        this->last_voted = temp;
+      }
+      else
+      {
+        bool flag = this->check_last_voted(this->last_voted, temp);
+
+      }
     }
     else if(inputMessageVector[0] == "release")
     {
-      pop_messageQ(this->threadId);
+      this->pop_messageQ();
       this->voted = false;
     }
   }
 }
 
+//Function to check for grants
 void Node::check_grants()
 {
-  if(Node::grantQ[this->threadId].size() == 4)
-    this->myState == HELD;
+  if(Node::grantQ[this->threadId].size() == 5)
+  {
+    this->myState = HELD;
+    this->logged = true;
+  }
 }
 
+//Function to log a message to stdout before enter the critical section
 void Node::logging()
 {
   ostringstream ss;
-  time_t logTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::time_t logTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-  ss << "Time: " << ctime(&logTime) << ", ThreadId: " << this->threadId << ", Node-List: " ;
-
-  for (int i=0; i<this->requestSet.size(); i++)
-  {
-    ss << this->requestSet[i] << " ";
-  }
-
-  std::cout << ss.str() << std::endl;
+  std::cout << "Time: " << ctime(&logTime) << ", ThreadId: " << this->threadId << ", Node-List: " << this->requestSetString << std::endl;
 }
 
+//Function to simulate each node/thread
 void node_runner(const int i)
 {
   std::chrono::system_clock::time_point start, cs_start, release_start;
@@ -189,43 +247,77 @@ void node_runner(const int i)
   start = std::chrono::system_clock::now();
 
   //std::cout << "Hi, I am node: " << node_id << std::endl;
+
+  //NODE INITIALIZATION
   Node newnode = Node(i+1);
   newnode.display();
-  while( 1 )
+  while( !newnode.expired )
   {
+    //check for program time elapse
     timer = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
 
     if(static_cast<int>(timer.count()) >= Node::total_exec_time )
-      break;
+      newnode.expired = true;
 
+    // cs_start = std::chrono::system_clock::now();
+    // srand(time(0));
+    // int breaker = std::rand() % 1000*i;
+    // while(1)
+    // {
+    //   cs_timer = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - cs_start);
+    //
+    //   if(static_cast<int>(cs_timer.count()) >= breaker )
+    //     break;
+    //   newnode.check_messages();
+    // }
+
+    //NODE REQUEST STAGE
     newnode.myState = REQUEST;
     newnode.multicast(newnode.myState);
 
-    while(newnode.myState == REQUEST)
+    while(newnode.myState == REQUEST && !newnode.expired)
     {
       newnode.check_messages();
       newnode.check_grants();
+
+      timer = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
+
+      if(static_cast<int>(timer.count()) >= Node::total_exec_time )
+        newnode.expired = true;
     }
 
-    // Each thread printing a log to the screen before it enters its crtical section
-    newnode.logging();
+    //NODE CRITICAL SECTION STAGE
 
+    // Each thread printing a log to the screen before it enters its crtical section
+    if(newnode.logged)
+    {
+      newnode.logging();
+    }
+
+    newnode.clear_grantQ();
     cs_start = std::chrono::system_clock::now();
 
-    while(newnode.myState == HELD)
+    while(newnode.myState == HELD && !newnode.expired)
     {
       // wait for cs_int seconds
       cs_timer = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - cs_start);
 
       if(static_cast<int>(cs_timer.count()) >= Node::cs_int )
         break;
+
+      timer = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
+
+      if(static_cast<int>(timer.count()) >= Node::total_exec_time )
+        newnode.expired = true;
     }
 
+    //NODE RELEASE STAGE
+    newnode.logged = false;
     newnode.myState = RELEASE;
     newnode.multicast(newnode.myState);
     release_start = std::chrono::system_clock::now();
 
-    while(newnode.myState == RELEASE)
+    while(newnode.myState == RELEASE && !newnode.expired)
     {
       //wait for next_req seconds
       release_timer = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - release_start);
@@ -233,33 +325,42 @@ void node_runner(const int i)
       if(static_cast<int>(release_timer.count()) >= Node::next_req )
         break;
 
+      timer = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
+
+      if(static_cast<int>(timer.count()) >= Node::total_exec_time )
+        newnode.expired = true;
+
       newnode.check_messages();
+
+
     }
   }
 }
 
+//voting set map for each node based on a 3x3 grid
 void init_voting_set()
 {
-  votingSet[1] = {2,3,4,7};
-  votingSet[2] = {1,3,5,8};
-  votingSet[3] = {1,2,6,9};
-  votingSet[4] = {1,5,6,7};
-  votingSet[5] = {2,4,6,8};
-  votingSet[6] = {3,4,5,9};
-  votingSet[7] = {1,4,8,9};
-  votingSet[8] = {2,5,7,9};
-  votingSet[9] = {3,6,7,8};
-
+  votingSet[1] = {1,2,3,4,7};
+  votingSet[2] = {1,2,3,5,8};
+  votingSet[3] = {1,2,3,6,9};
+  votingSet[4] = {1,4,5,6,7};
+  votingSet[5] = {2,4,5,6,8};
+  votingSet[6] = {3,4,5,6,9};
+  votingSet[7] = {1,4,7,8,9};
+  votingSet[8] = {2,5,7,8,9};
+  votingSet[9] = {3,6,7,8,9};
 }
+
 /*
  * Function - Main function to map commands, spawn node 0 and spawn listener thread.
  */
 int main(int argc, char *argv[])
 {
+  beginning_of_time =  std::chrono::system_clock::now();
   init_voting_set();
   // <cs_int>	 <next_req>	  <tot_exec_time>	  <option>
   int N = 9;
-  if(argc >=5 )
+  if(argc > 5 || argc < 4 )
   {
     std::cout << "\nPlease enter the right number of arguements\n" << std::endl;
     std::cout << "\n./mutex	<cs_int> <next_req> <tot_exec_time> <option> " << std::endl;
@@ -275,6 +376,13 @@ int main(int argc, char *argv[])
     ss2 >> Node::next_req;
     ss3 >> Node::total_exec_time;
 
+    if(argc == 5)
+    {
+      istringstream ss4(argv[4]);
+      ss4 >> Node::option;
+      std::cout << "option is set: " << typeid(Node::option).name() << std::endl;
+    }
+
   }
   // display startup message
   std::cout << "\n=======================" << std::endl;
@@ -287,7 +395,7 @@ int main(int argc, char *argv[])
     thread_pool[i] = std::thread(node_runner, i);
   }
   // wait for all the node threads to complete their execution
-  //std::cout << "Joining" << std::endl;
+  //std::cout << "Closing all nodes as total execution time has elapsed" << std::endl;
   for(int i=0; i<9; i++)
   {
     thread_pool[i].join();
--- headers/Node.h
+++ headers/Node.h
@@ -15,8 +15,14 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <mutex>
+#include <thread>
+#include <ctime>
+#include <ratio>
+#include <chrono>
 
 enum State { INIT, REQUEST, HELD, RELEASE };
+typedef std::pair <std::string, std::chrono::system_clock::time_point> MessageTimePair;
 
 class Node
 {
@@ -26,25 +32,36 @@ private:
   std::vector<int> requestSet;
   std::string lastReply;
   bool voted;
+  std::string requestSetString;
+  MessageTimePair last_voted;
 
 public:
   Node(const int);
   State myState;
+  bool expired;
+  bool logged;
   static int cs_int;
   static int next_req;
   static int total_exec_time;
-
+  static int option;
   static std::vector<int> member_list;
-  static std::vector<std::deque<std::string>> messageQ;
+  static std::vector<std::mutex> mtx_locks;
+  static std::vector<std::deque<MessageTimePair>> messageQ;
   static std::vector<std::vector<std::string>> grantQ;
 
   void init();
   void check_messages();
   void check_grants();
+  bool check_last_voted(const MessageTimePair, const MessageTimePair);
   void multicast(const State);
   void yield();
   void display();
   void logging();
+
+  void pop_messageQ();
+  void push_messageQ(const int, const MessageTimePair, const State);
+  void clear_grantQ();
+  void push_grantQ(const int, const std::string);
 };
 
 #endif  /* NODE_H_ */
